a984791574ea685001af6342ffec741fe3b22248
uki0327 <uki0327@gmail.com>
2020-12-06 21:46:47 +0900

    comparator match interrupt -> make the interrupt service routine short

--- comparator_match_interrupt2/comparator_match_interrupt2/main.c
+++ comparator_match_interrupt2/comparator_match_interrupt2/main.c
@@ -12,12 +12,34 @@
 volatile int count = 0;
 int state = 0;
 
+ISR(TIMER0_COMPA_vect) {
+	count++;
+	TCNT0 = 0;
+	
+}
 
 int main(void)
 {
+	DDRD = 0xFF;
+	PORTD = 0xFF;
+	
+	TCCR0B |= (1 << CS02) | (1 << CS00);
+	
+	OCR0A = 128;
+	
+	TIMSK0 |= (1 << OCIE0A);
+	sei();
 	
 	while (1)
 	{
+		if(count==64) {
+			count = 0;
+			state = !state;
+			if(state) PORTD = 0x00;
+			else PORTD = 0xFF;
+		}
+		
 	}
 }
+
 
