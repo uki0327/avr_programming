97af9c39bcd9a3bb694cd60b409650e16dd8a824
uki0327 <uki0327@gmail.com>
2020-12-06 21:33:05 +0900

    comparator match interrupt source code coding

---
+++ comparator_match_interrupt2/comparator_match_interrupt2/main.c
@@ -0,0 +1,23 @@
+/*
+* comparator_match_interrupt2.c
+*
+* Created: 2020-12-06 오후 9:12:12
+* Author : justin
+*/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define F_CPU 16000000L
+
+volatile int count = 0;
+int state = 0;
+
+
+int main(void)
+{
+	
+	while (1)
+	{
+	}
+}
+
