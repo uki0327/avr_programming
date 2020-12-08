e8ecb163070a18a845e3b79d2377199515f35ed9
uki0327 <uki0327@gmail.com>
2020-12-08 20:38:44 +0900

    ADC 포텐셔미터로 LED ON 갯수 바꾸기

---
+++ ADC_potentiometer_led_count/ADC_potentiometer_led_count/main.c
@@ -0,0 +1,50 @@
+/*
+ * ADC_potentiometer_led_count.c
+ *
+ * Created: 2020-12-08 오후 8:19:41
+ * Author : PROCESSOR
+ */ 
+#define F_CPU 16000000L
+#include <avr/io.h>
+#include <util/delay.h>
+
+void ADC_INIT(unsigned char channel) {
+	ADMUX |= 0x40;
+	ADCSRA |= 0x07;
+	ADCSRA |= (1 << ADEN);
+	ADCSRA |= (1 << ADATE);
+	ADMUX |= ((ADMUX & 0xE0) | channel);
+	ADCSRA |= (1 << ADSC);
+}
+
+int read_ADC(void) {
+	while(!(ADCSRA & (1 << ADIF)));
+	
+	return ADC;
+}
+
+void PORT_INIT(void) {
+	DDRD = 0xFF;
+	PORTD = 0xFF;
+}
+int main(void)
+{
+    int value;
+	uint8_t on_off;
+	
+	ADC_INIT(0);
+	PORT_INIT();
+	
+	while (1) 
+    {
+		value = read_ADC() >> 7;
+		
+		on_off = 0;
+		for(int i = 0; i <= value ; i++) {
+			on_off |= (0x01 << i);
+		}
+    }
+	PORTD = ~on_off;
+}
+
+
