845fe8a351e1bd924dd7a09917add6363083c263
uki0327 <uki0327@gmail.com>
2020-12-08 19:29:29 +0900

    디바운싱 - 버튼이 완전히 눌렸을 때

--- debouncing_when_full_pressed/debouncing_when_full_pressed/main.c
+++ debouncing_when_full_pressed/debouncing_when_full_pressed/main.c
@@ -1,14 +1,16 @@
 /*
- * deboucing.c
- *
- * Created: 2020-12-08 오후 6:54:26
- * Author : PROCESSOR
- */ 
+* deboucing.c
+*
+* Created: 2020-12-08 오후 6:54:26
+* Author : PROCESSOR
+*/
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include "UART.h"
 #define F_CPU  16000000L
+#define bit_is_set(sfr, bit) (_SFR_BYTE(sfr) & _BV(bit)))
+#define bit_is_clear(sfr, bit) (!(_SFR_BYTE(sfr) & _BV(bit)))
 
 void INIT_PORT(void) {
 	DDRB = 0x20;
@@ -18,24 +20,34 @@ void INIT_PORT(void) {
 	PORTD = 0x04;
 }
 
+unsigned char button_pressed(void) {
+	if(bit_is_clear(PIND, PIND2)) { // 첫번째 검사
+		_delay_ms(10);
+		if(bit_is_clear(PIND, PIND2)) { // 두번째 검사
+			return 1;
+		}
+	}
+	return 0;
+}
+
+
+
 int main(void)
 {
 	UART_INIT();
 	INIT_PORT();
 	int state = 0;
-	    
-    while (1) 
-    {
-		if(PIND & 0x04) {
-			state = 0;
-			PORTB = 0x00;
-		} else {
+	
+	while (1)
+	{
+		if(button_pressed()) {
 			if(state == 0) UART_transmit('*');
 			state = 1;
 			PORTB = 0x20;
-			_delay_ms(100);
+			} else {
+			state = 0;
+			PORTB = 0x00;
 		}
-    }
-	
+	}
 	return 1;
 }
