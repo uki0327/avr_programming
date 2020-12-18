01475f41d8d9f9a96144a9fbdad767469433b617
uki0327 <uki0327@gmail.com>
2020-12-18 16:34:32 +0900

    정향교교수님 테스트

---
+++ Atmega128_20201218_Test_Q2/Atmega128_20201218_Test_Q2/main.c
@@ -0,0 +1,42 @@
+/*
+ * Atmega128_20201218_Test_Q2.c
+ *
+ * Created: 2020-12-18 오전 11:51:41
+ * Author : 이장욱
+ * [문제2] LED가 500ms 간격으로 (PC0 ~ PC7) 켜지도록 
+ * 반복하는 프로그램을 쉬프트 연산자를 활용하여 작성하세요.
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+#define F_CPU 16000000L
+
+#include "UART_USE_1.h"
+
+char ledState = 0xFE;
+
+void shiftLed() {
+	if(ledState == 0b01111111) {
+		ledState = 0b11111110;
+	} else {
+		ledState = (ledState << 1) + 1;
+	}
+}
+
+int main(void)
+{
+	UART_INIT(9600,2);
+	
+    DDRC = 0xFF;
+	PORTC = 0xFF;
+	
+    while (1) 
+    {
+		UART_print8bitBinaryInc0f(ledState);
+		UART_transmit('\n');
+		PORTC = ledState;
+		_delay_ms(5000);
+		shiftLed();
+    }
+}
+
