d091adc32f3b027d24d6bd7cf7ac83c29489ab6a
uki0327 <uki0327@gmail.com>
2020-12-20 16:34:07 +0900

    UBRRnL 값 구하는 수식 제거 후 하드코딩

--- UART_library/UART.c
+++ UART_library/UART.c
@@ -15,14 +15,55 @@ void UART_INIT(unsigned long boud, char x) {
 	
 	UBRR0H = 0x00;
 	
-	int xFactor;
+	/* int xFactor;
 	if (x == 1) xFactor = 16;	//	비동기 정상(1배속) 모드
 	else if (x == 2) xFactor = 8; // 비동기 2배속 모드
 	else if (x == 3) xFactor = 2; // 동기 마스터 모드
 	
 	double b;
 	b = F_CPU / (xFactor * boud) - 1 + 0.5; // 반올림을 위해 +0.5 적용한 계산
-	int UBRRnL_Value = (int)b; // 소수점 이하 자리 버림 (반올림)
+	int UBRRnL_Value = (int)b; // 소수점 이하 자리 버림 (반올림) */
+	
+	// UBRRnL 수식계산 결과값 오차 발생으로 하드코딩
+	int UBRRnL_Value;
+	if(x == 1) {
+		switch(boud) {
+			case 2400:		UBRRnL_Value =  416;	break;
+			case 4800:		UBRRnL_Value =  207;	break;
+			case 9600:		UBRRnL_Value =  103;	break;
+			case 14400:		UBRRnL_Value =  68;		break;
+			case 19200:		UBRRnL_Value =  51;		break;
+			case 28800:		UBRRnL_Value =  34;		break;
+			case 38400:		UBRRnL_Value =  25;		break;
+			case 57600:		UBRRnL_Value =  16;		break;
+			case 76800:		UBRRnL_Value =  12;		break;
+			case 115200:	UBRRnL_Value =  8;		break;
+			case 230400:	UBRRnL_Value =  3;		break;
+			case 250000:	UBRRnL_Value =  3;		break;
+			case 500000:	UBRRnL_Value =  1;		break;
+			case 1000000:	UBRRnL_Value =  0;		break;
+			default:		UBRRnL_Value =  103;	break;
+		}
+	} else if(x == 2) {
+		switch(boud) {
+			case 2400:		UBRRnL_Value =  832;	break;
+			case 4800:		UBRRnL_Value =  416;	break;
+			case 9600:		UBRRnL_Value =  207;	break;
+			case 14400:		UBRRnL_Value =  138;	break;
+			case 19200:		UBRRnL_Value =  103;	break;
+			case 28800:		UBRRnL_Value =  68;		break;
+			case 38400:		UBRRnL_Value =  51;		break;
+			case 57600:		UBRRnL_Value =  34;		break;
+			case 76800:		UBRRnL_Value =  25;		break;
+			case 115200:	UBRRnL_Value =  16;		break;
+			case 230400:	UBRRnL_Value =  8;		break;
+			case 250000:	UBRRnL_Value =  7;		break;
+			case 500000:	UBRRnL_Value =  3;		break;
+			case 1000000:	UBRRnL_Value =  1;		break;
+			default:		UBRRnL_Value =  207;	break;
+		}
+	}
+	
 	UBRR0L = UBRRnL_Value;
 	
 	UCSR0C |= 0x06;
